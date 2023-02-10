4a74ebf254ec0c3e19c689d2a3c5cac23d9ed369
Snehal979 <bansodsnehal535@gmail.com>
2023-02-10 13:59:58 +0530

    [Snehal]Add: Test Case -Find Max number.Given First Value Large and Return First Value

--- GenericFindMaxTestCase/UnitTest1.cs
+++ GenericFindMaxTestCase/UnitTest1.cs
@@ -5,7 +5,20 @@ namespace GenericFindMaxTestCase
     [TestClass]
     public class UnitTest1
     {
-       
-        
+        /// <summary>
+        /// Uc1.1 Given Max and return First test case
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxFirstNum_WhenAnalysed_ShouldReturnFistMaxNum()
+        {
+            FindMaxNumber findMaxNumber = new FindMaxNumber();
+            //AAA
+            //arrage initializatiion of vaiables
+            int result = findMaxNumber.FindMaxValue_Int(30, 2, 5);
+            // act creation of object and calling particular 
+            //assert validation the output
+            Assert.AreEqual(30, result);
+        }
+
     }
 }
