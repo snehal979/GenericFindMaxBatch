1163eeb4d870ca3877bfc696ce144a72a9f215ae
Snehal979 <bansodsnehal535@gmail.com>
2023-02-10 13:39:01 +0530

    [Snehal]Add: Welcom msg for generic

---
+++ GenericFindMax/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericFindMax
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Generic Find Maximun Number");
+        }
+    }
+}
